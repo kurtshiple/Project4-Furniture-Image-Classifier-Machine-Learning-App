96bb41a5084ac76de88dc49770d0ad11441a5957
jchung243 <jchung243@gmail.com>
2019-07-16 20:09:30 -0400

    upload works

--- web/app.py
+++ web/app.py
@@ -1,11 +1,14 @@
 # import necessary libraries
-from flask import Flask, render_template, jsonify
-from werkzeug import secure_filename
-import requests
+from flask import Flask, render_template, jsonify, request, redirect, url_for
+from werkzeug.utils import secure_filename
 import os
 
 # create instance of Flask app
 app = Flask(__name__)
+
+UPLOAD_FOLDER = '/path/to/the/uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+app.config['UPLOAD_FOLDER'] = "./static/img/"
 
 # create route that renders index.html template
 @app.route("/")
@@ -16,15 +19,22 @@ def home():
 # def input():
 #     return render_template("index.html")
 
-@app.route("/upload/",methods=['GET','POST'])
+@app.route("/upload", methods=['GET','POST'])
 def upload_file():
     if request.method == 'POST':
-        file = request.files['file[]']
-    if file:
-            filename = secure_filename(file.filename)
-            file.save(os.path.join('../static/img',filename))
-            return render_template('output.html')
-    return render_template('output.html')
+        f = request.files['file']
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename)))
+        return render_template("output.html")
+    return render_template("output.html")
+
+    # if request.method == 'POST':
+    #     file = request.files['file[]']
+    # if file:
+    #         filename = secure_filename(file.filename)
+    #         file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+    #         return render_template('output.html')
+    # return render_template('output.html')
+
 # def upload_file():
 #     if request.method =='POST':
 #         file = request.files['file']
@@ -44,9 +54,9 @@ def upload_file():
 #     elif request.method == 'GET':
 #         return render_template('contact.html', form=form)
 
-@app.route("/output")
-def output():
-    return render_template("index.html")
+# @app.route("/output")
+# def output():
+#     return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
